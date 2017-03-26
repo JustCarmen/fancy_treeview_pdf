8981f7c542f006b32c21e9b3deea88d416b0e377
JustCarmen <carmen@justcarmen.nl>
2017-03-26 14:14:43 +0200

    Short array syntax

--- app/FancyTreeviewPdfClass.php
+++ app/FancyTreeviewPdfClass.php
@@ -58,8 +58,8 @@ class FancyTreeviewPdfClass extends FancyTreeviewPdfModule {
 		$sortname	 = $person->getSortName();
 		$text1		 = I18N::translateContext('Unknown given name', '…');
 		$text2		 = I18N::translateContext('Unknown surname', '…');
-		$search		 = array(',', '@P.N.', '@N.N.');
-		$replace	 = array(', ', $text1, $text2);
+		$search		 = [',', '@P.N.', '@N.N.'];
+		$replace	 = [', ', $text1, $text2];
 		return str_replace($search, $replace, $sortname);
 	}
 
