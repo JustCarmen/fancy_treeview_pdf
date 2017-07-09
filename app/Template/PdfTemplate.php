753fb670272cbe2e60637bda11770146a73e9be9
JustCarmen <carmen@justcarmen.nl>
2017-07-10 00:25:53 +0200

    Update mpdf library to version 7.0.0-beta2 for PHP 7 compatibility

--- app/Template/PdfTemplate.php
+++ app/Template/PdfTemplate.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * webtrees: online genealogy
- * Copyright (C) 2017 webtrees development team
  * Copyright (C) 2017 JustCarmen
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,89 +19,81 @@ use Fisharebest\Webtrees\Filter;
 use Fisharebest\Webtrees\I18N;
 use Fisharebest\Webtrees\User;
 use JustCarmen\WebtreesAddOns\FancyTreeviewPdf\FancyTreeviewPdfClass;
-use mPDF;
+use Mpdf\Mpdf;
 
 class PdfTemplate extends FancyTreeviewPdfClass {
 
-	public function pageBody() {
-		$tmp_dir = WT_DATA_DIR . 'ftv_pdf_tmp/';
+  public function pageBody() {
+    $tmp_dir = WT_DATA_DIR . 'ftv_pdf_tmp/';
 
-		define('_JPGRAPH_PATH', $tmp_dir);
-		define('_MPDF_TEMP_PATH', $tmp_dir);
-		define('_MPDF_TTFONTDATAPATH', $tmp_dir);
+    require_once(WT_MODULES_DIR . $this->getName() . '/mpdf/vendor/autoload.php');
 
-		require_once(WT_MODULES_DIR . $this->getName() . '/mpdf/mpdf.php');
+    $stylesheet     = file_get_contents($this->directory . '/css/style.css');
+    $stylesheet_rtl = file_get_contents($this->directory . '/css/style-rtl.css');
 
-		$stylesheet		 = file_get_contents($this->directory . '/css/style.css');
-		$stylesheet_rtl	 = file_get_contents($this->directory . '/css/style-rtl.css');
+    $html = Filter::post('pdfContent');
 
-		$html = Filter::post('pdfContent');
+    $header = '<header>=== ' . $this->tree()->getTitleHtml() . ' ===</header>';
+    $footer = '<footer>' .
+        '<table><tr>' .
+        '<td class="left">' . WT_BASE_URL . '</td>' .
+        '<td class="center">{DATE d-m-Y}</td>' .
+        '<td class="right">{PAGENO}</td>' .
+        '</tr></table>' .
+        '</footer>';
 
-		$header	 = '<header>=== ' . $this->tree()->getTitleHtml() . ' ===</header>';
-		$footer	 = '<footer>' .
-			'<table><tr>' .
-			'<td class="left">' . WT_BASE_URL . '</td>' .
-			'<td class="center">{DATE d-m-Y}</td>' .
-			'<td class="right">{PAGENO}</td>' .
-			'</tr></table>' .
-			'</footer>';
+    $mpdf = new Mpdf([
+        'tempDir'              => $tmp_dir,
+        'simpleTables'         => true,
+        'shrink_tables_to_fit' => 1,
+        'autoScriptToLang'     => true,
+        'autoLangToFont'       => true,
+        'setAutoTopMargin'     => 'stretch',
+        'setAutoBottomMargin'  => 'stretch',
+        'autoMarginPadding'    => 5
+    ]);
 
-		$mpdf = new mPDF();
+    if (I18N::direction() === 'rtl') {
+      $mpdf->SetDirectionality('rtl');
+      $mpdf->WriteHTML($stylesheet_rtl, 1);
+    } else {
+      $mpdf->WriteHTML($stylesheet, 1);
+    }
 
-		$mpdf->simpleTables			 = true;
-		$mpdf->shrink_tables_to_fit	 = 1;
+    $admin = User::find($this->tree()->getPreference('WEBMASTER_USER_ID'))->getRealName();
 
-		$mpdf->autoScriptToLang	 = true;
-		$mpdf->baseScript		 = 1;
-		$mpdf->autoVietnamese	 = true;
-		$mpdf->autoArabic		 = true;
+    $mpdf->setCreator($this->getTitle() . ' - a webtrees module by justcarmen.nl');
+    $mpdf->SetTitle(Filter::get('title'));
+    $mpdf->setAuthor($admin);
 
-		$mpdf->autoLangToFont = true;
-		if (I18N::direction() === 'rtl') {
-			$mpdf->SetDirectionality('rtl');
-			$mpdf->WriteHTML($stylesheet_rtl, 1);
-		} else {
-			$mpdf->WriteHTML($stylesheet, 1);
-		}
+    $mpdf->SetHTMLHeader($header);
+    $mpdf->setHTMLFooter($footer);
 
-		$mpdf->setAutoTopMargin		 = 'stretch';
-		$mpdf->setAutoBottomMargin	 = 'stretch';
-		$mpdf->autoMarginPadding	 = 5;
+    $html_chunks = explode("\n", $html);
+    $chunks      = count($html_chunks);
+    $i           = 1;
+    foreach ($html_chunks as $html_chunk) {
+      // write html body parts only (option 2);
+      if ($i === 1) {
+        // first chunk (initialize all buffers - init=true)
+        $mpdf->WriteHTML($html_chunk, 2, true, false);
+      } elseif ($i === $chunks) {
+        // last chunck (close all buffers - close=true)
+        $mpdf->WriteHTML($html_chunk, 2, false, true);
+      } else {
+        // all other parts (keep the buffer open)
+        $mpdf->WriteHTML($html_chunk, 2, false, false);
+      }
+      $i++;
+    }
 
-		$admin = User::find($this->tree()->getPreference('WEBMASTER_USER_ID'))->getRealName();
-
-		$mpdf->setCreator($this->getTitle() . ' - a webtrees module by justcarmen.nl');
-		$mpdf->SetTitle(Filter::get('title'));
-		$mpdf->setAuthor($admin);
-
-		$mpdf->SetHTMLHeader($header);
-		$mpdf->setHTMLFooter($footer);
-
-		$html_chunks = explode("\n", $html);
-		$chunks		 = count($html_chunks);
-		$i			 = 1;
-		foreach ($html_chunks as $html_chunk) {
-			// write html body parts only (option 2);
-			if ($i === 1) {
-				// first chunk (initialize all buffers - init=true)
-				$mpdf->WriteHTML($html_chunk, 2, true, false);
-			} elseif ($i === $chunks) {
-				// last chunck (close all buffers - close=true)
-				$mpdf->WriteHTML($html_chunk, 2, false, true);
-			} else {
-				// all other parts (keep the buffer open)
-				$mpdf->WriteHTML($html_chunk, 2, false, false);
-			}
-			$i++;
-		}
-
-		$index = '
+    $index = '
 				<pagebreak type="next-odd" />
 				<h2>' . I18N::translate('Index') . '</h2>
 				<columns column-count="2" column-gap="5" />
 				<indexinsert usedivletters="on" links="on" collation="' . WT_LOCALE . '.utf8" collationgroup="' . I18N::collation() . '" />';
-		$mpdf->writeHTML($index);
-		$mpdf->Output($tmp_dir . Filter::get('title') . '.pdf', 'F');
-	}
+    $mpdf->writeHTML($index);
+    $mpdf->Output($tmp_dir . Filter::get('title') . '.pdf', 'F');
+  }
 
 }
